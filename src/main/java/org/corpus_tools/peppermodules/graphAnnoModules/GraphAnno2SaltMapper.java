e4d4379442eefd6207d23a793ffbd1bad481a0d0
Thomas Krause <krauseto@hu-berlin.de>
2021-08-16 12:59:34 +0200

    Map tokens of the graph

--- src/main/java/org/corpus_tools/peppermodules/graphAnnoModules/GraphAnno2SaltMapper.java
+++ src/main/java/org/corpus_tools/peppermodules/graphAnnoModules/GraphAnno2SaltMapper.java
@@ -1,12 +1,65 @@
 package org.corpus_tools.peppermodules.graphAnnoModules;
 
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.corpus_tools.pepper.common.DOCUMENT_STATUS;
 import org.corpus_tools.pepper.impl.PepperMapperImpl;
+import org.corpus_tools.pepper.modules.exceptions.PepperModuleException;
+import org.corpus_tools.peppermodules.graphAnnoModules.model.Edge;
+import org.corpus_tools.peppermodules.graphAnnoModules.model.EdgeType;
+import org.corpus_tools.peppermodules.graphAnnoModules.model.Node;
+import org.corpus_tools.peppermodules.graphAnnoModules.model.NodeType;
+import org.corpus_tools.peppermodules.graphAnnoModules.model.PartFile;
+import org.corpus_tools.salt.common.SDocumentGraph;
+import org.corpus_tools.salt.common.STextualDS;
 
 public class GraphAnno2SaltMapper extends PepperMapperImpl {
   @Override
   public DOCUMENT_STATUS mapSDocument() {
-    // TODO implement
-    return DOCUMENT_STATUS.COMPLETED;
+    
+    // Load JSON file
+    try (FileReader reader =
+        new FileReader(new File(getResourceURI().toFileString()),
+            StandardCharsets.UTF_8)) {
+      Gson gson = new Gson();
+      PartFile partFile = gson.fromJson(reader, PartFile.class);
+
+      mapToken(partFile);
+
+      return DOCUMENT_STATUS.COMPLETED;
+    } catch (IOException ex) {
+      throw new PepperModuleException("Could not read part file " + getResourceURI(), ex);
+    }
   }
+
+  private void mapToken(PartFile f) {
+    Map<Long, Node> tokenById = f.getNodes().parallelStream().filter(n -> n.getType() == NodeType.t)
+        .collect(Collectors.toMap(n -> n.getId(), n -> n));
+    // Sort all ordering edges by their start node index
+    Comparator<Edge> startCmp = Comparator.comparingLong(Edge::getStart);
+    List<Edge> orderingEdges =
+        f.getEdges().parallelStream().filter(e -> e.getType() == EdgeType.o)
+            .filter(e -> tokenById.containsKey(e.getStart()) && tokenById.containsKey(e.getEnd()))
+            .sorted(startCmp).collect(Collectors.toList());
+    
+    // Create token strings for each ordering edges
+    List<String> tokenTexts = orderingEdges.stream().map(e -> e.getStart())
+        .map(id -> (String) tokenById.get(id).getAttr().get("token")).collect(Collectors.toList());
+    // Add last token
+    tokenTexts.add((String) tokenById.get(orderingEdges.get(orderingEdges.size() - 1).getEnd())
+        .getAttr().get("token"));
+    
+    //
+    SDocumentGraph g = getDocument().getDocumentGraph();
+    STextualDS ds = g.createTextualDS("");
+    g.insertTokensAt(ds, 0, tokenTexts, true);
+  }
+
 }
--- src/test/java/org/corpus_tools/peppermodules/graphAnnoModules/GraphAnnoImporterTest.java
+++ src/test/java/org/corpus_tools/peppermodules/graphAnnoModules/GraphAnnoImporterTest.java
@@ -1,14 +1,18 @@
 package org.corpus_tools.peppermodules.graphAnnoModules;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.util.List;
 import org.corpus_tools.pepper.common.CorpusDesc;
 import org.corpus_tools.pepper.testFramework.PepperImporterTest;
 import org.corpus_tools.pepper.testFramework.PepperTestUtil;
 import org.corpus_tools.salt.common.SCorpus;
 import org.corpus_tools.salt.common.SCorpusGraph;
 import org.corpus_tools.salt.common.SDocument;
+import org.corpus_tools.salt.common.SDocumentGraph;
+import org.corpus_tools.salt.common.SToken;
 import org.eclipse.emf.common.util.URI;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,15 +22,15 @@ public class GraphAnnoImporterTest extends PepperImporterTest {
   @Before
   public void setUp() throws Exception {
     setFixture(new GraphAnnoImporter());
+    File testFolder = new File(PepperTestUtil.getTestResources() + "sampleCorpus/");
+    getFixture().setCorpusDesc(
+        new CorpusDesc().setCorpusPath(URI.createFileURI(testFolder.getAbsolutePath())));
   }
 
   @Test
   public void testImportCorpusStructureSCorpusGraph() {
-    File testFolder = new File(PepperTestUtil.getTestResources() + "sampleCorpus/");
-    getFixture().setCorpusDesc(
-        new CorpusDesc().setCorpusPath(URI.createFileURI(testFolder.getAbsolutePath())));
-    start();
 
+    start();
 
     // This should have created a root corpus and two documents
     assertEquals(1, getFixture().getSaltProject().getCorpusGraphs().size());
@@ -43,7 +47,36 @@ public class GraphAnnoImporterTest extends PepperImporterTest {
     assertEquals(2, cg.getDocuments().size());
     SDocument doc2 = cg.getDocuments().get(1);
     assertEquals("0002", doc2.getName());
+  }
 
+  @Test
+  public void testTokenization() {
+    start();
+
+    assertEquals(2, getFixture().getSaltProject().getCorpusGraphs().get(0).getDocuments().size());
+    SDocument doc = getFixture().getSaltProject().getCorpusGraphs().get(0).getDocuments().get(0);
+    SDocumentGraph g = doc.getDocumentGraph();
+    
+    assertNotNull(g);;
+    
+    
+    assertEquals(1, g.getTextualDSs().size());
+    assertEquals("Is this example more complicated than it appears to be ? ",
+        g.getTextualDSs().get(0).getText());
+
+    List<SToken> token = doc.getDocumentGraph().getSortedTokenByText();
+    assertEquals(11, token.size());
+    assertEquals("Is", g.getText(token.get(0)));
+    assertEquals("this", g.getText(token.get(1)));
+    assertEquals("example", g.getText(token.get(2)));
+    assertEquals("more", g.getText(token.get(3)));
+    assertEquals("complicated", g.getText(token.get(4)));
+    assertEquals("than", g.getText(token.get(5)));
+    assertEquals("it", g.getText(token.get(6)));
+    assertEquals("appears", g.getText(token.get(7)));
+    assertEquals("to", g.getText(token.get(8)));
+    assertEquals("be", g.getText(token.get(9)));
+    assertEquals("?", g.getText(token.get(10)));
   }
 
 }
